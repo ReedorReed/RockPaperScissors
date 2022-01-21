26099ffa3edee1404b8fd7741e9b70f4a207726f
Christian Reed <chreed87@gmail.com>
2022-01-21 16:03:50 +0100

    Made the app return randomly Rock, Paper or scissors

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const rps = ['Rock', 'Paper', 'Scissors'];
+
+
 function computerPlay() {
-    
+    const randomNumber = Math.floor(Math.random() * rps.length);
+    const choice = rps[randomNumber];
+
+    return choice;
 }
+
