7d31fecc0ef79c0d6ebfc426a6f15d000ec4593b
Christian Reed <chreed87@gmail.com>
2022-01-28 21:32:24 +0100

    Trying to add a new function that plays a game

--- app.js
+++ app.js
@@ -8,3 +8,32 @@ function computerPlay() {
     return choice;
 }
 
+function playRound (playerSelection, computerSelection) {
+    // possibly use if statements 
+    if ('rock' == 'scissors' || 'paper' == 'rock' || 'scissors' == 'paper') {
+        return ('You lose! Paper beats rock');
+    }
+
+}
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+console.log(playRound());
+
+
+
+
+
+
+// const rock > scissors ; 
+// const paper > rock ;
+// const scissors > paper ;
+
+// function hi(c, b) {
+//     c = ('hello ');
+//     b = ('world!');
+
+//     return (c+b);
+// }
+
+
