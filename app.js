92c9a39e9e47f75d07ab9ebf97045f79eea2da4c
Christian Reed <chreed87@gmail.com>
2022-02-05 15:59:45 +0100

    added a loop and got the game to loop 5 times and keep score

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const rps = ['rock', 'paper', 'scissors'];
 let playerSelection = prompt('What do you choose?').toLowerCase();
 let computerSelection = computerPlay();
+let playerScore = 0;
+let computerScore = 0;
+let draws = 0;
+let lose = 'You lose!';
+let win = 'You win!';
+let draw = 'Draw!';
 
 
 
@@ -15,30 +21,66 @@ function computerPlay() {
 // Play a round of rock, paper or scissors
 function playRound (playerSelection, computerSelection) {
     
-    if (playerSelection === 'rock' & computerSelection === 'paper') {
-        return ('You lose! Paper beats Rock');
+    if (playerSelection === computerSelection) {
+        return (draw);
+    } else if (playerSelection === 'rock' & computerSelection === 'paper') {
+        computerScore++;
+        return (lose + ' Paper beats Rock');
     } else if (playerSelection === 'rock' & computerSelection === 'scissors') {
-        return ('You win! Rock beats Scissors');
+        playerScore++;
+        return (win + ' Rock beats Scissors');
     } else if (playerSelection === 'paper' & computerSelection === 'scissors') {
-        return ('You lose! Scissors beats Paper');
+        computerScore++;
+        return (lose + ' Scissors beats Paper');
     } else if (playerSelection === 'paper' & computerSelection === 'rock') {
-        return ('You win! Paper beats Rock');
+        playerScore++;
+        return (win + ' Paper beats Rock');
     } else if (playerSelection === 'scissors' & computerSelection === 'rock') {
-        return ('You lose! Rock beats Scissors');
+        computerScore++;
+        return (lose + ' Rock beats Scissors');
     } else if (playerSelection === 'scissors' & computerSelection === 'paper') {
-        return ('You win! Scissors beats Paper');
+        playerScore++;
+        return (win + ' Scissors beats Paper');
     }
 
 }
 
 console.log(playRound(playerSelection, computerSelection));
 
+// Function that keeps score of game
 
+function gameScore(){
+    let result = playRound();
+    if (result === win) {
+        playerScore++;
+    } else if (result = draw) {
+        draws++;
+    } else {
+        computerScore++;
+    }
+    
+    if (playerScore == 5) {
+        console.log(win);
+        return;
+    }
+    if (computerScore == 5) {
+        console.log(lose);
+        return;
+    }
+    }
 
 // Create a function called game that loops the game 5 times and keeps score
 
-function game(){
+for (let i = 0; i < 5; i++) {
+    let roundResult = playRound(playerSelection, computerSelection);
+    console.log(roundResult);
+    gameScore(roundResult);
+    console.log('Your score is ' + playerScore);
+    console.log('Computer score is ' + computerScore);
 
+    if (playerScore === 5 || computerScore === 5) {
+        break;
+    }
 }
 
 
